6ea9f6637094ee43283a10a8f6a05310318207ae
Minecraft0088 <wiktorasonovic@outlook.com>
2024-09-08 00:34:32 +0400

    RABBIT MQ SIMPLE CONNECT

---
+++ Rabbit_MQ_simple/Rabbit_MQ_simple/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Hosting;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace Rabbit_MQ_simple
+{
+
+	internal class Program
+	{
+		//public static IModel IModel { get; private set; }
+
+		static void Main(string[] args)
+		{
+			var factory = new ConnectionFactory() { HostName = "localhost" };
+			using (var con = factory.CreateConnection())
+			{
+				using (var chanel = con.CreateModel())
+				{
+					chanel.QueueDeclare(queue: "first",
+						exclusive: false,
+						durable: true,
+						autoDelete: false,
+						arguments: null);
+
+					var message = "message-75211-52-cv-DE-777-REM";
+					var body = Encoding.UTF8.GetBytes(message);
+
+					chanel.BasicPublish(exchange: "",
+						routingKey: "first",
+						basicProperties: null,
+						body: body);
+
+					Console.WriteLine("DONE");
+				}
+
+			}
+			Console.ReadLine();
+		}
+
+	}
+}
---
+++ Rabbit_MQ_simple/Rabbit_mq_get_msg/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+
+namespace Rabbit_mq_get_msg
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var factory = new ConnectionFactory() { HostName = "localhost" };
+			using (var con = factory.CreateConnection())
+			{
+				using (var chanel = con.CreateModel())
+				{
+					chanel.QueueDeclare(queue: "first",
+						exclusive: false,
+						durable: true,
+						autoDelete: false,
+						arguments: null);
+
+					var consumer = new EventingBasicConsumer(chanel);
+
+					consumer.Received += (model, es) =>
+					{
+						var body = es.Body.ToArray();
+						var message = Encoding.UTF8.GetString(body);
+						Console.WriteLine($"Received message: {message}");
+					};
+
+					chanel.BasicConsume(queue: "first",
+						autoAck: true,
+						consumer: consumer);
+
+					Console.WriteLine("Waiting for messages. Press [enter] to exit.");
+					Console.ReadLine(); // Prevent program from exiting immediately
+				}
+			}
+		}
+	}
+}
